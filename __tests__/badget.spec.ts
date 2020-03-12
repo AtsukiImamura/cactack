8b7cd841c22dd97e70e79f29f97dd344b409a80b
Atsuki Imamura <ohmoon.au@gmail.com>
2020-03-13 00:35:08 +0900

    予算のテストシナリオ作成

---
+++ __tests__/badget.spec.ts
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+import DependencyInjectionConfig from "../src/config/DependencyInjectionConfig";
+import StabBadgetRepository from "../src/repository/stab/StabBadgetRepository";
+import StabBadgetGroupRepository from "../src/repository/stab/StabBadgetGroupRepository";
+
+// テストの前のセッティング
+beforeAll(() => {
+  DependencyInjectionConfig.runTest();
+  (container.resolve("BadgetRepository") as StabBadgetRepository).clearAll();
+  (container.resolve(
+    "BadgetGroupRepository"
+  ) as StabBadgetGroupRepository).clearAll();
+});
+
+import { container } from "tsyringe";
+import BadgetService from "../src/service/BadgetService";
+
+test("create new badget", done => {
+  container
+    .resolve(BadgetService)
+    .createNewBadgetGroup("家賃")
+    .then(group => {
+      return container
+        .resolve(BadgetService)
+        .createThisMonthsBadget(group, 2500);
+    })
+    .then(badget => {
+      expect(badget.id).toBe("1");
+      done();
+    })
+    .catch(err => {
+      done(err);
+    });
+});
